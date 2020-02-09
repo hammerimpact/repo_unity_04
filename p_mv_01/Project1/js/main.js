ea5183bb1da51df4d1cc159ed1596b5e9f231e59
HammerMan <hammerimpact88@gmail.com>
2020-02-09 21:16:40 +0900

    [RMMV] Merge Plugins

--- p_mv_01/Project1/js/main.js
+++ p_mv_01/Project1/js/main.js
@@ -34229,6 +34229,305 @@ Window_DebugEdit.prototype.updateVariable = function() {
 //#endregion rpg_windows.js
 
 
+//#region Community_Basic.js
+(function () {
+
+    // Setting : For setting the upper limit of image memory cache. (MPix)
+    var cacheLimit = 10;
+
+    // Setting : For setting the screen width.
+    var screenWidth = 816;
+
+    // Setting : For setting the screen height.
+    var screenHeight = 624;
+
+    // Setting : Rendering mode (canvas/webgl/auto)
+    var renderingMode = "auto";
+
+    // Setting : To set initial value as to whether the player always dashes. (on/off)
+    var alwaysDash = "off";
+
+    // Setting : If set, change window width to this value
+    var windowWidthTo = 0;
+
+    // Setting : If set, change window height to this value
+    var windowHeightTo = 0;
+
+    var windowWidth;
+    var windowHeight;
+
+    if (windowWidthTo) {
+        windowWidth = windowWidthTo;
+    } else if (screenWidth !== SceneManager._screenWidth) {
+        windowWidth = screenWidth;
+    }
+
+    if (windowHeightTo) {
+        windowHeight = windowHeightTo;
+    } else if (screenHeight !== SceneManager._screenHeight) {
+        windowHeight = screenHeight;
+    }
+
+
+    ImageCache.limit = cacheLimit * 1000 * 1000;
+    SceneManager._screenWidth = screenWidth;
+    SceneManager._screenHeight = screenHeight;
+    SceneManager._boxWidth = screenWidth;
+    SceneManager._boxHeight = screenHeight;
+
+    SceneManager.preferableRendererType = function () {
+        if (Utils.isOptionValid('canvas')) {
+            return 'canvas';
+        } else if (Utils.isOptionValid('webgl')) {
+            return 'webgl';
+        } else if (renderingMode === 'canvas') {
+            return 'canvas';
+        } else if (renderingMode === 'webgl') {
+            return 'webgl';
+        } else {
+            return 'auto';
+        }
+    };
+
+    var _ConfigManager_applyData = ConfigManager.applyData;
+    ConfigManager.applyData = function (config) {
+        _ConfigManager_applyData.apply(this, arguments);
+        if (config['alwaysDash'] === undefined) {
+            this.alwaysDash = alwaysDash;
+        }
+    };
+
+
+    var _SceneManager_initNwjs = SceneManager.initNwjs;
+    SceneManager.initNwjs = function () {
+        _SceneManager_initNwjs.apply(this, arguments);
+
+        if (Utils.isNwjs() && windowWidth && windowHeight) {
+            var dw = windowWidth - window.innerWidth;
+            var dh = windowHeight - window.innerHeight;
+            window.moveBy(-dw / 2, -dh / 2);
+            window.resizeBy(dw, dh);
+        }
+    };
+})();
+//#endregion Community_Basic.js
+
+
+
+//#region MadeWithMv.js
+var Liquidize = Liquidize || {};
+Liquidize.MadeWithMV = {};
+
+// Setting : Enabled/Disables showing the "Made with MV" splash screen.
+Liquidize.MadeWithMV.ShowMV = true;
+
+// Setting : The image to use when showing "Made with MV"
+Liquidize.MadeWithMV.MVImage = "MadeWithMv";
+
+// Setting : Enabled/Disables showing the "Made with MV" splash screen.
+Liquidize.MadeWithMV.ShowCustom = false;
+
+// Setting : The image to use when showing "Made with MV"
+Liquidize.MadeWithMV.CustomImage = "";
+
+// Setting :  The time it takes to fade out, in frames.
+Liquidize.MadeWithMV.FadeOutTime = 120;
+
+// Seting : The time it takes to fade in, in frames.
+Liquidize.MadeWithMV.FadeInTime = 120;
+
+// Setting : The time between fading in and out, in frames.
+Liquidize.MadeWithMV.WaitTime = 160;
+
+//-----------------------------------------------------------------------------
+// Scene_Splash
+//
+// This is a constructor, implementation is done in the inner scope.
+
+function Scene_Splash() {
+    this.initialize.apply(this, arguments);
+}
+
+(function() {
+
+    //-----------------------------------------------------------------------------
+    // Scene_Boot
+    //
+    // The scene class for dealing with the game boot.
+    
+    var _Scene_Boot_loadSystemImages = Scene_Boot.prototype.loadSystemImages;
+    Scene_Boot.prototype.loadSystemImages = function() {
+        _Scene_Boot_loadSystemImages.call(this);
+        if (Liquidize.MadeWithMV.ShowMV) {
+            ImageManager.loadSystem(Liquidize.MadeWithMV.MVImage);
+        }
+        if (Liquidize.MadeWithMV.ShowCustom) {
+            ImageManager.loadSystem(Liquidize.MadeWithMV.CustomImage);
+        }
+    };
+
+    var _Scene_Boot_start = Scene_Boot.prototype.start;
+    Scene_Boot.prototype.start = function() {
+        if ((Liquidize.MadeWithMV.ShowMV || Liquidize.MadeWithMV.ShowCustom) && !DataManager.isBattleTest() && !DataManager.isEventTest()) {
+            SceneManager.goto(Scene_Splash);
+        } else {
+            _Scene_Boot_start.call(this);
+        }
+    };
+
+    //-----------------------------------------------------------------------------
+    // Scene_Splash
+    //
+    // The scene class for dealing with the splash screens.
+
+    Scene_Splash.prototype = Object.create(Scene_Base.prototype);
+    Scene_Splash.prototype.constructor = Scene_Splash;
+
+    Scene_Splash.prototype.initialize = function() {
+        Scene_Base.prototype.initialize.call(this);
+        this._mvSplash = null;
+        this._customSplash = null;
+        this._mvWaitTime = Liquidize.MadeWithMV.WaitTime;
+        this._customWaitTime = Liquidize.MadeWithMV.WaitTime;
+        this._mvFadeOut = false;
+        this._mvFadeIn = false;
+        this._customFadeOut = false;
+        this._customFadeIn = false;
+    };
+
+    Scene_Splash.prototype.create = function() {
+        Scene_Base.prototype.create.call(this);
+        this.createSplashes();
+    };
+
+    Scene_Splash.prototype.start = function() {
+        Scene_Base.prototype.start.call(this);
+        SceneManager.clearStack();
+        if (this._mvSplash != null) {
+            this.centerSprite(this._mvSplash);
+        }
+        if (this._customSplash != null) {
+            this.centerSprite(this._customSplash);
+        }
+    };
+
+    Scene_Splash.prototype.update = function() {
+        if (Liquidize.MadeWithMV.ShowMV) {
+            if (!this._mvFadeIn) {
+                this.startFadeIn(Liquidize.MadeWithMV.FadeInTime, false);
+                this._mvFadeIn = true;
+            } else {
+                if (this._mvWaitTime > 0 && this._mvFadeOut == false) {
+                    this._mvWaitTime--;
+                } else {
+                    if (this._mvFadeOut == false) {
+                        this._mvFadeOut = true;
+                        this.startFadeOut(Liquidize.MadeWithMV.FadeOutTime, false);
+                    }
+                }
+            }
+        }
+
+        if (Liquidize.MadeWithMV.ShowCustom) {
+            if (Liquidize.MadeWithMV.ShowMV && this._mvFadeOut == true) {
+                if (!this._customFadeIn && this._fadeDuration == 0) {
+                    this._customSplash.opacity = 255;
+                    this._customWaitTime = Liquidize.MadeWithMV.WaitTime;
+                    this.startFadeIn(Liquidize.MadeWithMV.FadeInTime, false);
+                    this._customFadeIn = true;
+                } else {
+                    if (this._customWaitTime > 0 && this._customFadeOut == false) {
+                        this._customWaitTime--;
+                    } else {
+                        if (this._customFadeOut == false) {
+                            this._customFadeOut = true;
+                            this.startFadeOut(Liquidize.MadeWithMV.FadeOutTime, false);
+                        }
+                    }
+                }
+            } else if (!Liquidize.MadeWithMV.ShowMV) {
+                if (!this._customFadeIn) {
+                    this._customSplash.opacity = 255;
+                    this.startFadeIn(Liquidize.MadeWithMV.FadeInTime, false);
+                    this._customFadeIn = true;
+                } else {
+                    if (this._customWaitTime > 0 && this._customFadeOut == false) {
+                        this._customWaitTime--;
+                    } else {
+                        if (this._customFadeOut == false) {
+                            this._customFadeOut = true;
+                            this.startFadeOut(Liquidize.MadeWithMV.FadeOutTime, false);
+                        }
+                    }
+                }
+            }
+        }
+
+        if (Liquidize.MadeWithMV.ShowCustom) {
+            if (Liquidize.MadeWithMV.ShowMV && this._mvFadeOut == true && this._customFadeOut == true) {
+                this.gotoTitleOrTest();
+            } else if (!Liquidize.MadeWithMV.ShowMV && this._customFadeOut == true) {
+                this.gotoTitleOrTest();
+            }
+        } else {
+            if (this._mvFadeOut == true) {
+                this.gotoTitleOrTest();
+            }
+        }
+
+        Scene_Base.prototype.update.call(this);
+    };
+
+    Scene_Splash.prototype.createSplashes = function() {
+        if (Liquidize.MadeWithMV.ShowMV) {
+            this._mvSplash = new Sprite(ImageManager.loadSystem(Liquidize.MadeWithMV.MVImage));
+            this.addChild(this._mvSplash);
+        }
+        if (Liquidize.MadeWithMV.ShowCustom) {
+            this._customSplash = new Sprite(ImageManager.loadSystem(Liquidize.MadeWithMV.CustomImage));
+            this._customSplash.opacity = 0;
+            this.addChild(this._customSplash);
+        }
+    };
+
+    Scene_Splash.prototype.centerSprite = function(sprite) {
+        sprite.x = Graphics.width / 2;
+        sprite.y = Graphics.height / 2;
+        sprite.anchor.x = 0.5;
+        sprite.anchor.y = 0.5;
+    };
+
+    Scene_Splash.prototype.gotoTitleOrTest = function() {
+        Scene_Base.prototype.start.call(this);
+        SoundManager.preloadImportantSounds();
+        if (DataManager.isBattleTest()) {
+            DataManager.setupBattleTest();
+            SceneManager.goto(Scene_Battle);
+        } else if (DataManager.isEventTest()) {
+            DataManager.setupEventTest();
+            SceneManager.goto(Scene_Map);
+        } else {
+            this.checkPlayerLocation();
+            DataManager.setupNewGame();
+            SceneManager.goto(Scene_Title);
+            Window_TitleCommand.initCommandPosition();
+        }
+        this.updateDocumentTitle();
+    };
+
+    Scene_Splash.prototype.updateDocumentTitle = function() {
+        document.title = $dataSystem.gameTitle;
+    };
+
+    Scene_Splash.prototype.checkPlayerLocation = function() {
+        if ($dataSystem.startMapId === 0) {
+            throw new Error('Player\'s starting position is not set');
+        }
+    };
+
+})();
+//#endregion MadeWithMv.js
+
 
 //=============================================================================
 // main.js
